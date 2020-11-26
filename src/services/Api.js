f6d0422e11c9736be644944170985b6497db700b
Eduardo Dedios <eduardodediosp96@gmail.com>
2020-11-26 14:05:16 -0500

    adding config to master

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const HTTP = axios.create({
     // baseURL: 'https://localhost:44368/api',
-    baseURL: 'https://traininggainapi20201124030845.azurewebsites.net/api/',
+    baseURL: 'https://traininggainapi20201124030845.azurewebsites.net/api',
     // baseURL: 'https://traininggainapidevelop20201124002700.azurewebsites.net/api/',
     headers: {
         'Accept': 'application/json',
